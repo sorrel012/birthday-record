8d15a812fa70c6f3325cb0e4c044df3be7317425
sorrel012 <wain1719@gmail.com>
2024-04-17 17:47:42 +0900

    [24.04.17] Header 설정(임시)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Header from '@/components/Header';
+
 export default function Home() {
   return (
+    <>
+      <Header />
     <main>
       하잉
     </main>
+    </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,11 @@
+import { Monofett } from 'next/font/google';
+import '../styles/globals.css';
+
+const monofett = Monofett({
+  weight: '400', subsets: ["latin"] });
+
 function Header() {
-  return <header></header>;
+  return <h1 className={`${monofett.className} title`}>Birthday Record</h1>;
 }
 
 export default Header;
