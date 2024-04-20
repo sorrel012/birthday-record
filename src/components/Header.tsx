23411bb3e8d6c6314b6a32173956a762e43fb8d4
sorrel012 <wain1719@gmail.com>
2024-04-20 21:50:11 +0900

    [24.04.20] 헤더 반응형 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 
 function Header() {
   return (
-    <div className="text-center pt-10">
+    <div className="text-center pt-5 sm:pt-10">
       <h1 className={`title`}>Birthday Record</h1>
     </div>
   );
